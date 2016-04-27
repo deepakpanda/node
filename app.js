d2db347c395ce651b699ec05415293adc2898be1
deepakpanda <deepak.panda@hpe.com>
2016-04-27 17:18:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-var http = require('http');
-var express = require('express');
-var app = express();
+var express  = require('express');
+var app      = express();
+//var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path = require('path'),
+    fs = require('fs');
+ var http = require('http')
+var server = http.createServer(app)
 
+
+var configDB = require('./config/database.js');
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+mongoose.connect(configDB.url); 
+
+require('./config/passport')(passport); 
+
+app.configure(function() {
+
+	app.use(express.cookieParser());
+	app.use(express.bodyParser()); 
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.set('views', __dirname + '/views');
+	app.engine('html', require('ejs').renderFile);
+	app.use(express.session({ secret: 'knoldus' })); 
+	app.use(express.bodyParser({uploadDir:'/images'}));
+	app.use(passport.initialize());
+	app.use(passport.session()); 
+	app.use(flash()); 
+
+});
+
+
+
+require('./app/routes.js')(app, passport,server); 
+
+server.listen(port);
+console.log('Listening  to  port ' + port);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
