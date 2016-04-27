f5871a7bcfba58f8be6721585b9956dc33c7692a
deepakpanda <deepak.panda@hpe.com>
2016-04-27 16:12:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+//var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -11,6 +11,12 @@ var server = http.createServer(app)
 
 
 var configDB = require('./config/database.js');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 mongoose.connect(configDB.url); 
 
@@ -30,12 +36,7 @@ app.configure(function() {
 	app.use(flash()); 
 
 });
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
 
 
 require('./app/routes.js')(app, passport,server); 
